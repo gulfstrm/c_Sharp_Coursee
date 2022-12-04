7af08e7d0985d12f0d1d9e4ba4a0d069a6ae43a0
stas <gulfstrm@rambler.ru>
2022-12-04 19:11:59 +0300

    из трех большее 1_6

--- 1_lesson/1_6/Program.cs
+++ 1_lesson/1_6/Program.cs
@@ -1,15 +1,26 @@
 ﻿// Напишите программу ,которая принимаетна вход три числа и выдает
 // максимальное из этих чисел.
-Console.WriteLine("Введите число 1");
-int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число  1");
+int a = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число 2");
-int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число  2");
+int b = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число 3");
-int num3 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число  3");
+int c = Convert.ToInt32(Console.ReadLine());
 
-if(num1>num2);
+if(a>b)
 {
-    
+    Console.WriteLine(a);
 }
+
+else if(a>c)
+{
+    Console.WriteLine(a);
+}
+else if(b>c)
+{
+    Console.WriteLine(b);
+}
+else 
+Console.WriteLine(c);
